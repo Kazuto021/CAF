04784ead616780700e1cef3159f48d890606a126
Kazu <kshitizprof@gmail.com>
2024-04-24 22:55:41 +0530

    added gojo and luffy bannerCards

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,54 @@ const App = () => {
       <BannerCard
         title={Bannerinfo.naruto.Name}
         mycolor="#782DFF"
-        from="#fccb90"
-        to="#d57eeb"
+
+        degree={Bannerinfo.naruto.gradient.degree}
+
+        from={Bannerinfo.naruto.gradient.from}
+        to={Bannerinfo.naruto.gradient.to}
+        startOpacity={Bannerinfo.naruto.gradient.startOpacity}
+        endOpacity={Bannerinfo.naruto.gradient.endOpacity}
         imgSrc={Bannerinfo.naruto.Img}
         quote={Bannerinfo.naruto.Quote}
-        heading = {Bannerinfo.naruto.Heading}
+        heading={Bannerinfo.naruto.Heading}
       />
       <BannerCard
         title={Bannerinfo.sao.Name}
         mycolor="#c81325c9"
+        degree={Bannerinfo.sao.gradient.degree}
         from={Bannerinfo.sao.gradient.from}
+        startOpacity={Bannerinfo.sao.gradient.startOpacity}
+        endOpacity={Bannerinfo.sao.gradient.endOpacity}
         to={Bannerinfo.sao.gradient.to}
         imgSrc={Bannerinfo.sao.Img}
         quote={Bannerinfo.sao.Quote}
-        heading = {Bannerinfo.sao.Heading}
+        heading={Bannerinfo.sao.Heading}
+
+      />
+      <BannerCard
+        title={Bannerinfo.jjk.Name}
+        mycolor="#c81325c9"
+        degree={Bannerinfo.jjk.gradient.degree}
+        from={Bannerinfo.jjk.gradient.from}
+        startOpacity={Bannerinfo.jjk.gradient.startOpacity}
+        endOpacity={Bannerinfo.jjk.gradient.endOpacity}
+        to={Bannerinfo.jjk.gradient.to}
+        imgSrc={Bannerinfo.jjk.Img}
+        quote={Bannerinfo.jjk.Quote}
+        heading={Bannerinfo.jjk.Heading}
+      />
+
+      <BannerCard
+        title={Bannerinfo.onepiece.Name}
+        mycolor="#c81325c9"
+        degree={Bannerinfo.onepiece.gradient.degree}
+        from={Bannerinfo.onepiece.gradient.from}
+        startOpacity={Bannerinfo.onepiece.gradient.startOpacity}
+        endOpacity={Bannerinfo.onepiece.gradient.endOpacity}
+        to={Bannerinfo.onepiece.gradient.to}
+        imgSrc={Bannerinfo.onepiece.Img}
+        quote={Bannerinfo.onepiece.Quote}
+        heading={Bannerinfo.onepiece.Heading}
 
       />
     </>
--- src/utils/BannerUtil.jsx
+++ src/utils/BannerUtil.jsx
@@ -5,8 +5,11 @@ const Bannerinfo = {
         Quote: "All The Time. I Just Wanted To Be With You. You Changed Me! Your Smile Is What Saved Me! That Is Why I’m Not Afraid To Die Protecting You!",
         Heading: "SOMEBODY TOLD ME I’M A FAILURE, I’LL PROVE THEM WRONG",
         gradient: {
-            from:"#fccb90",
-            to:"#d57eeb"
+            degree: "120deg",
+            from: "#fccb90",
+            startOpacity: "0",
+            to: "#d57eeb",
+            endOpacity: "100"
         }
     },
     sao: {
@@ -15,11 +18,42 @@ const Bannerinfo = {
         Heading: "DO THE UNTHINKABLE",
         Quote: "This may be a virtual world, but I feel more alive here than in the real world.",
         gradient: {
-            from:"#00c6fb",
-            to:"#005bea"
+            degree: "120deg",
+            from: "#00c6fb",
+            startOpacity: "0",
+            to: "#005bea",
+            endOpacity: "100"
+        }
+    },
+    jjk: {
+        Name: "Jujutsu Kaisen",
+        Img: "/gojo2.png",
+        Heading: "Strongest Sorcerer",
+        Quote: "Throughout Heaven and Earth, I Alone Am The Honored One",
+        gradient: {
+            degree: "120deg",
+            from: "#fbc8d4",
+            startOpacity: "0",
+            to: "#120d2f",
+            endOpacity: "95"
+
+        }
+
+    },
+    onepiece: {
+        Name: "One Piece",
+        Img: "/luffygear5.png",
+        Heading: "KING OF THE PIRATES",
+        Quote: "Pirates will have a banquet and dig right in, but a hero would give it to other people. Screw that — My meat Is mine!",
+        gradient: {
+            degree: "to right",
+            from: "#d7d2cc",
+            startOpacity: "0",
+            to: "#304352",
+            endOpacity: "100"
+
         }
     }
-
 }
 
 export default Bannerinfo
