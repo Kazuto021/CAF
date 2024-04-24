31ecf7551cca284f14d7783cfdfc3899749d4587
Kazu <kshitizprof@gmail.com>
2024-04-24 14:36:43 +0530

    improved design and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import BannerCard from "./Components/BannerCard";
+import Bannerinfo from "./utils/BannerUtil";
 
 const App = () => {
   return (
-    <div>This is a trial statement.</div>
-  )
-}
+    <>
 
-export default App
+      <BannerCard
+        title={Bannerinfo.naruto.Name}
+        mycolor="#782DFF"
+        from="#fccb90"
+        to="#d57eeb"
+        imgSrc={Bannerinfo.naruto.Img}
+        quote={Bannerinfo.naruto.Quote}
+        heading = {Bannerinfo.naruto.Heading}
+      />
+      <BannerCard
+        title={Bannerinfo.sao.Name}
+        mycolor="#c81325c9"
+        from={Bannerinfo.sao.gradient.from}
+        to={Bannerinfo.sao.gradient.to}
+        imgSrc={Bannerinfo.sao.Img}
+        quote={Bannerinfo.sao.Quote}
+        heading = {Bannerinfo.sao.Heading}
+
+      />
+    </>
+  );
+};
+
+export default App;
