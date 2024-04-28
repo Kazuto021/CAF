d98413f51e7c2b245e232b5a78edf998db8d2900
Kazu <kshitizprof@gmail.com>
2024-04-28 17:04:57 +0530

    HomePage Creation

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -8,7 +8,8 @@ const Content = (props) => {
         <div className='content-wrapper'>
             <div className='anime-info'>
                 <h1>
-                    {props.name}
+                    {/* {console.log(typeof(props.name))} */}
+                    {(props.name).toUpperCase()}
                 </h1>
                 <p>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, rem aliquid dignissimos quod doloremque pariatur cumque magnam consequatur tempora architecto vero iste nam consequuntur suscipit magni, aut mollitia consectetur illum.
@@ -24,11 +25,7 @@ const Content = (props) => {
                 <div className='carousel'>
                     <h2 className='currently-showing'>Currently Viewing</h2>
                     <div className='current-card' style={{"backgroundImage":`url(${props.bgImg})`}}></div>
-                    <div className='controllers'>
-                        <div className="right-arrow">
-                            <FaArrowRight />
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
 
